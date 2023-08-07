4d9059bffd0a9999602d7fefae5628c40aad8b7c
Emma <77967471+draumaz@users.noreply.github.com>
2023-08-06 21:55:46 -0700

    ok

---
+++ sillyutils.sh
@@ -0,0 +1,108 @@
+#!/bin/bash -e
+
+oae() {
+  MACPAC_REPO="https://oooa.draumaz.xyz/app" MACPAC_INSTALL_PATH="/Applications" \
+    macpac "${@}"
+}  
+
+corntab() {
+  sp="/-\|"
+  case $1 in -e) google-chrome-stable https://www.youtube.com/watch?v=X0YLt4dcx8w; exit 0 ;; esac
+  clear; cat << EOF
+=================
+corntab
+by draumaz
+=================
+EOF
+  read; clear
+}
+
+macpac_index_regenerate() {
+
+  WORKING="/var/oooa/bin"
+  cd "${WORKING}"
+  
+  rm -f ./index.html
+  case $1 in no-pre)echo ok;;*)
+  cat >> index.html << EOF
+<html lang="en">
+  
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"">
+	<title>opt-out-of-air/news</title>
+  </head>
+  
+  <main class="container">
+    <hgroup>
+      <hgroup>
+        <h2>opt-out-of-air/bin</h2>
+        <h3><a href="/">home</a> · <a href="/news">news</a> · bin</h3>
+      </hgroup> <br>
+    
+EOF
+  ;; esac
+  for PKG in $(find * -name "*.tar.gz" | sort); do
+    cat >> index.html << EOF
+      <a href="https://oooa.draumaz.xyz/bin/${PKG}">${PKG}</a> <br>
+EOF
+    echo "<a href=https://oooa.draumaz.xyz/bin/${PKG}>${PKG}</a> <br>"; done
+
+    cat >> index.html << EOF
+    </hgroup>
+  </main>
+</html>
+EOF
+
+}
+
+hdd_backup() {
+  DATE=`date -I`
+  case ${1} in dots)
+    case $1 in --include-repos) XTRA="remote-repos" ;; *) ;; esac
+    FILE="$HOME/dots-${DATE}-${HOSTNAME}.tar"
+    cd ${HOME}
+    tar -cvf ${FILE} \
+      .git* \
+      .hidden* \
+      .ssh* \
+      ${XTRA}
+ ;; storage)
+	  FILE="/mnt/storage/storage-${DATE}-${HOSTNAME}.tar"
+	  cd /mnt/storage
+	  tar -cvf ${FILE} \
+		misc \
+		music \
+		photos \
+		unix \
+		videos \
+		vpn
+  ;; esac
+  du -sh "${FILE}"
+}
+
+fastpush() {
+  case "${1}" in '') exit 1 ;; *) cd "${1}" ;; esac
+  git add *; git commit -m "${2}"; git push
+}
+
+tbc() {
+  case ${URL} in "") URL="${1}" ;; esac 
+  curl -sL ${URL} | grep 'watch?v' | \
+    tr '"' '\n' | grep 'youtube.com/watch?v' | tee -a ./videos.txt
+}
+
+glog() {
+  CMD="tail -f /var/log/portage/*"${@}"*"
+  case $(id -u) in 0) "${CMD}" ;; *) sudo "${CMD}" ;; esac
+}
+
+loop_despook() {
+  while ps aux | grep -i "[M]icrosoftEdgeUpdate" > /dev/null 2>&1; do
+    kill `ps aux | grep -i "[M]icrosoftEdgeUpdate" | awk '{print $2}'`
+    echo "found and killed."
+  done
+}
+
+"${@}"
